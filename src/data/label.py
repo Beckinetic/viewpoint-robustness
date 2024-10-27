318644a4bd658b4ee351aba125a5dc4ba90f0724
TTK <tsotsengkun@gmail.com>
2024-10-27 14:31:35 +0100

    test

--- src/data/label.py
+++ src/data/label.py
@@ -18,10 +18,22 @@ data_folders = config['data_folders']
 
 
 def relabel():
+    def process_folder(folder_path):
+        # check if there are any image files in the current folder
+        if any(file.endswith(('.png', '.jpg', '.jpeg')) for file in os.listdir(folder_path) if
+               os.path.isfile(os.path.join(folder_path, file))):
+            create_labels(folder_path)
+            print(f'Relabelled {folder_path}')
+
+        # recursively check all subdirectories
+        for subfolder in os.listdir(folder_path):
+            subfolder_path = os.path.join(folder_path, subfolder)
+            if os.path.isdir(subfolder_path):
+                process_folder(subfolder_path)
+
     for data_folder in data_folders:
         data_path = os.path.join(root, data_folder)
-        create_labels(data_path)
-        print('Done')
+        process_folder(data_path)
 
 
 if __name__ == '__main__':
